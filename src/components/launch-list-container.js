fc62ac76530e1662d3147d2dce919ae5f0c32b33
sadamant <sadamant@student.42.us.org>
2018-11-11 00:18:19 +0100

    Move out data grabbing to container

--- src/components/launch-list-container.js
+++ src/components/launch-list-container.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import axios from 'axios';
 import LaunchList from './launch-list';
 
 class LaunchListContainer extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			items: []
+		};
+	}
+
+	// GET request to the spacexdata api, grab out all the required data and store
+	// it in an array.
+	componentWillMount() {
+		var launches = [];
+		axios.get('https://api.spacexdata.com/v3/launches/past')
+			.then((req) => {
+				this.setState({"items": req.data});
+				req.data.forEach(obj => {
+					var singleLaunch = {};
+					singleLaunch['launch_year'] = obj.launch_year;
+					singleLaunch['mission_name'] = obj.mission_name;
+					singleLaunch['launch_site_name'] = obj.launch_site.site_name_long;
+					singleLaunch['payload_mass_kg'] = obj.rocket.second_stage.payloads[0].payload_mass_kg;
+					singleLaunch['land_success'] = String(obj.rocket.first_stage.cores[0].land_success);
+					launches = [...launches, singleLaunch];
+				})
+				this.setState({"items": launches});
+			})
+			.catch(err => {
+				console.log(err)
+			})
+	}
 
 	render() {
 		return (
@@ -14,7 +44,7 @@ class LaunchListContainer extends React.Component {
 				"width": "100%",
 				"overflowY": "scroll",
 				}}>
-					<LaunchList />
+					<LaunchList items={this.state.items}/>
 				</div>
 		);
 	}
--- src/components/launch-list.js
+++ src/components/launch-list.js
@@ -6,7 +6,6 @@ class LaunchList extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			items: [],
 			selected_fields: {
 				launch_year: '-',
 				land_success: '-',
@@ -16,7 +15,7 @@ class LaunchList extends React.Component {
 	}
 
 	computedFields(selected_fields) {
-		return this.state.items.filter(obj => {
+		return this.props.items.filter(obj => {
 			if (selected_fields.land_success === '-'
 				&& selected_fields.launch_year === '-') {
 					return obj;
@@ -40,34 +39,11 @@ class LaunchList extends React.Component {
 		}, () => console.log(this.state))
 	}
 
-	// GET request to the spacexdata api, grab out all the required data and store
-	// it in an array.
-	componentWillMount() {
-		var launches = [];
-		axios.get('https://api.spacexdata.com/v3/launches/past')
-			.then((req) => {
-				this.setState({"items": req.data});
-				req.data.forEach(obj => {
-					var singleLaunch = {};
-					singleLaunch['launch_year'] = obj.launch_year;
-					singleLaunch['mission_name'] = obj.mission_name;
-					singleLaunch['launch_site_name'] = obj.launch_site.site_name_long;
-					singleLaunch['payload_mass_kg'] = obj.rocket.second_stage.payloads[0].payload_mass_kg;
-					singleLaunch['land_success'] = String(obj.rocket.first_stage.cores[0].land_success);
-					launches = [...launches, singleLaunch];
-				})
-				this.setState({"items": launches});
-			})
-			.catch(err => {
-				console.log(err)
-			})
-	}
-
 	render() {
 		return (
 			<table>
 				<LaunchListTableHeader
-					fields={this.state.items}
+					fields={this.props.items}
 					onChangeFieldsSelected={this.onChangeFieldsSelected}/>
 				<tbody>
 					{this.computedFields(this.state.selected_fields).map((obj, index) => (
